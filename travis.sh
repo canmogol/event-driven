cfb079c3c2b042387a61fe1218f9cb4850df6dfa
Can A. MOGOL <canmogol@gmail.com>
2016-02-22 11:45:28 +0200

    travis.sh re-arranged

--- travis.sh
+++ travis.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
+sudo apt-get install libprotobuf-dev libprotobuf-java protobuf-compiler
 #sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 #sudo apt-get update -qq
 #sudo apt-get install -qq g++-4.8
 #sudo apt-get install libprotobuf-dev libprotobuf-java protobuf-compiler
 #sudo apt-get install --no-install-recommends
-sudo apt-get install libprotobuf-dev libprotobuf-java protobuf-compiler
