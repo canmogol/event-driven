eb050fdf7c171e9f95115404d0bb932930983d5a
canmogol <canmogol@gmail.com>
2016-03-01 14:00:55 +0200

    travis will build for java

--- travis.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-#sudo apt-get update -qq
-#sudo apt-get install -qq g++-4.8
-#sudo apt-get install libprotobuf-dev libprotobuf-java protobuf-compiler
-#sudo apt-get install --no-install-recommends
-sudo apt-get install libprotobuf-dev libprotobuf-java protobuf-compiler
